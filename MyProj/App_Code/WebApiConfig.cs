573811c1be519478a4d89f2b818dc732abbad661
Turcian Darius <turciandarius@yahoo.com>
2017-12-11 21:51:30 +0200

    Am adaugat posibilitatea de la lua  informatii despre vreme

---
+++ MyProj/App_Code/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+/// <summary>
+/// Summary description for WebApiConfig
+/// </summary>
+public class WebApiConfig
+{
+    public static void Register(HttpConfiguration config)
+    {
+        config.MapHttpAttributeRoutes();
+
+        config.Routes.MapHttpRoute(
+            name: "4GApi",
+            routeTemplate: "4gapi/"
+        );
+    }
+}
